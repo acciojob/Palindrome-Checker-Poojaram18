149f2e5bd37c679991840f93035168970de0fb47
PoojaRamaraj <119429335+Poojaram18@users.noreply.github.com>
2023-01-17 09:11:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-// This file is not to be modified. Please ignore this.
-// We will understand all of this later in the course.
-// DO NOT MODIFY THIS FILE
+// complete the given function
 
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser')
+function palindrome(str){
+	if(str.length== 0){
+		return false;
+	}
+	
+	let left =0;
+	let right = str.length-1;
+	while(left<right){
+		if(str[left]!==str[right]){
+			return false;
+		}
+		left++;
+		right--;
+		}
+	return true;
+	}
 
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-const isPalindrome = require('./script');
-
-app.use(express.static(__dirname))
-
-app.post('/palindromechecker',(req, res) => {
-  const str = req.body.str
-  const answer = isPalindrome(str) 
-  res.send({message:answer})
-})
-
-module.exports = app;
+module.exports = palindrome
